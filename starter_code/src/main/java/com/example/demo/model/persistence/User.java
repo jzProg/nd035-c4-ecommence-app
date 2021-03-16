f4e7a288479729e42f0f190314769594ded98879
jzProg <john_z94@hotmail.com>
2021-03-16 21:19:51 +0200

    feat: jwt authentication implementation

--- starter_code/src/main/java/com/example/demo/model/persistence/User.java
+++ starter_code/src/main/java/com/example/demo/model/persistence/User.java
@@ -31,6 +31,17 @@ public class User {
     @JoinColumn(name = "cart_id", referencedColumnName = "id")
 	@JsonIgnore
     private Cart cart;
+
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@Column(nullable = false)
+	private String password;
+
+	public String getPassword(){
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
 	public Cart getCart() {
 		return cart;
